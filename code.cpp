10e18179719fee1e6a9cf4899afd4964914c140d
Saket Saumya <34890341+saket13@users.noreply.github.com>
2020-05-16 22:00:48 +0530

    Added the main template
    The main/basic template over which all templates are used...

---
+++ code.cpp
@@ -0,0 +1,85 @@
+//
+//   “Daḷhaṃ sikkhatha santiyā.” - Utthana Sutta (Sutta Nipata)
+//   "Life is a manifestation of where your energy is flowing" - Dandapani
+//    Sāket Sāumyā
+//    MAY 16 2020
+//
+
+#include<bits/stdc++.h>
+#include <string> 
+using namespace std;
+
+
+#define ll long long 
+#define all(x) x.begin(),x.end()
+#define endl '\n'
+#define RESET   "\033[0m"
+#define RED     "\033[31m"      /* Red */
+#define what_is(x) cerr << RED << #x << " is " << x  << RESET << endl;
+ 
+
+const long long MOD = 1e9+7 ;
+const int N=(1e3)+5;
+const long long INF=1LL << 57;
+ 
+// Data Structures
+vector <ll> vec(N),v1, v2, v3;
+vector < pair < ll , ll > > vp, vp1, vp2;
+set < ll > se, se1;
+unordered_set <ll > use;
+multiset < ll > ms;
+map <  ll, ll > mp, done, mp1;
+unordered_map < string, ll > ump;
+
+
+ll t, n, m, p, q, x, y, a, b, c, mini= LLONG_MAX, maxi= LLONG_MIN,res, ans;
+
+
+queue <pair <ll, ll > > bfsq;
+ll vis[N];
+vector < pair <ll, ll> >  adj[N], subtree[N];
+ll visited[1005][1005] ;
+char mat[1005][1005];
+ll dp[4005];
+
+
+//<--------------------------------------------------------------------------------------------------------------------------->//
+
+
+signed  main(){
+            
+            ios_base::sync_with_stdio(false);
+            cin.tie(nullptr);cout.tie(nullptr);
+        
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}       
+       
+ 
+ 
+       
+       
+       
+       
